4b43305296d8ef2aa8397ead1350c97d4fea06ba
aurorasphere <kraftwerkislove@gmail.com>
2025-04-22 18:53:47 +0000

    a little bit of code

--- src/main.zig
+++ src/main.zig
@@ -6,7 +6,6 @@ pub const SoC = struct {
     pc: u24,
     instruction_memory: [16777216]u8,
     data_memory: [16777216]u8,
-    fn execR
 };
 
 fn fetch(self: *SoC) u32 {
@@ -24,6 +23,25 @@ fn DecodeAndExecute(self: *SoC, instr: u32) void {
         0b0000000 => {
             self.execR(instr);
         },
+    }
+}
+
+fn execR(self: *SoC, instr: u32) void {
+    const rm = (instr >> 27) & 0b11111; // Rm
+    const rn = (instr >> 22) & 0b11111; // Rn
+    const rd = (instr >> 17) & 0b11111; // Rd
+    const fn3 = (instr >> 8) & 0b111; // fn3
+    const opcode = instr & 0b1111111; // opcode
+    //
+    switch (fn3) {
+        0b000 => self.regs[rd] = self.regs[rn] + self.regs[rm], // add
+        0b001 => self.regs[rd] = self.regs[rn] - self.regs[rm], // sub
+        0b010 => self.regs[rd] = self.regs[rn] | self.regs[rm], // or
+        0b011 => self.regs[rd] = self.regs[rn] & self.regs[rm], // and
+        0b100 => self.regs[rd] = self.regs[rn] ^ self.regs[rm], // xor
+        0b101 => self.regs[rd] = self.regs[rn] << self.regs[rm], // lsl
+        0b110 => self.regs[rd] = self.regs[rn] >> self.regs[rm], // lsr
+        else => @panic("Unknown R-type instruction"),
     }
 }
 
