7daaafc37372ec8181dde8a4dee37fc3aaf8603c
aurorasphere <kraftwerkislove@gmail.com>
2025-04-24 21:45:08 +0000

    yay

--- src/main.zig
+++ src/main.zig
@@ -7,6 +7,7 @@ pub const SoC = struct {
     instruction_memory: [16777216]u8,
     data_memory: [16777216]u8,
 
+    // Status register's flags
     pub const FLAG_EQ: u8 = 0b00000001;
     pub const FLAG_GT: u8 = 0b00000010;
     pub const FLAG_LT: u8 = 0b00000100;
@@ -93,6 +94,7 @@ fn ALU(self: *SoC, a: u32, b: u32, fn3: u8, fn7: u8) u32 {
             _ => result = 0,
         }
     }
+    return result;
 }
 
 fn fetch(self: *SoC) u32 {
@@ -124,7 +126,38 @@ fn execR(self: *SoC, instr: u32) void {
 }
 
 fn execI(self: *SoC, instr: u32) void {
-    const rm = (instr  >>)
+    const rm = (instr >> 27) & 0b11111;
+    const imm = (instr >> 15) & 0b1111_1111_1111;
+    const rd = (instr >> 10) & 0b11111;
+    const fn3 = (instr >> 7) & 0b111;
+    const opcode = instr & 0b1111111;
+
+    if (opcode == 1) {
+        self.regs[rd] = ALU(self, rm, imm, fn3, 0);
+    } else if (opcode == 9) {
+        const address = imm + rm;
+        switch (fn3) {
+            0b000 => { // Load word
+                const mem_word_data: u32 = (self.data_memory[address + 0] << 0 | self.data_memory[address + 1] << 8 | self.data_memory[address + 2] << 16 | self.data_memory[address + 3] << 24);
+                self.regs[rd] = mem_word_data;
+            },
+            0b001 => { // Load half
+                const mem_half_data = (self.data_memory[address + 0] << 0 | self.data_memory[address + 1]);
+                self.regs[rd] = mem_half_data;
+            },
+            0b011 => { // Load byte
+                self.regs[rd] = self.data_memory[address];
+            },
+            _ => @panic("Error: Invalid fn3 on I-Type Load instruction!\n"), // invalid operation
+        }
+    }
+}
+
+fn execS(self: *SoC, instr: u32) void {
+    const rm = (instr >> 27) & 0b11111;
+    const rn = (instr >> 22) & 0b11111;
+    const imm = (instr >> 10) & 0b1111_1111_1111;
+    const fn3 = (instr >> 7) & 0b111;
 }
 
 pub fn main() !void {}
