6f4889bb48dfc6f94f5536c983f3359f4b1cc7dd
aurorasphere <kraftwerkislove@gmail.com>
2025-04-24 20:59:51 +0000

    yippie

--- src/main.zig
+++ src/main.zig
@@ -16,6 +16,85 @@ pub const SoC = struct {
     pub const FLAG_SV: u8 = 0b11000000;
 };
 
+fn ALU(self: *SoC, a: u32, b: u32, fn3: u8, fn7: u8) u32 {
+    var result: u32 = 0;
+    fn7 &= 1;
+
+    if (fn7 == 0) {
+        switch (fn3) {
+            0b000 => { // add
+                result = a + b;
+
+                // Carry detection
+                if (result < a) {
+                    self.statusreg |= self.FLAG_C;
+                } else {
+                    self.statusreg &= ~self.FLAG_C;
+                }
+
+                // Overflow detection
+                const signed_a = (i32)(a);
+                const signed_b = (i32)(b);
+                const signed_result = (i32)(result);
+
+                if ((signed_a > 0 and signed_b > 0 and signed_result < 0) or (signed_a < 0 and signed_b < 0 and signed_result >= 0)) {
+                    self.statusreg |= self.FLAG_V;
+                } else {
+                    self.statusreg &= ~self.FLAG_V;
+                }
+            },
+            0b001 => result = a | b,
+            0b010 => result = a & b,
+            0b011 => result = a ^ b,
+            0b100 => result = a << b,
+            0b101 => result = a >> b,
+            0b110 => { // cmp
+                if (a == b) {
+                    self.statusreg |= self.FLAG_EQ;
+                } else {
+                    self.statusreg &= ~self.FLAG_EQ;
+                }
+
+                if (a > b) {
+                    self.statusreg |= self.FLAG_GT;
+                } else {
+                    self.statusreg &= ~self.FLAG_GT;
+                }
+
+                if (a < b) {
+                    self.statusreg |= self.FLAG_LT;
+                } else {
+                    self.statusreg &= ~self.FLAG_LT;
+                }
+            },
+            _ => result = 0,
+        }
+    } else if (fn7 == 1) {
+        switch (fn3) {
+            0b000 => { // sub
+                result = a - b;
+
+                // Carry detection
+                if (a < b) self.statusreg |= self.FLAG_C;
+
+                // Underflow detection
+                const signed_a = (i32)(a);
+                const signed_b = (i32)(b);
+                const signed_result = (i32)(result);
+
+                if ((signed_a >= 0 and signed_b < 0 and signed_result < 0) or (signed_a < 0 and signed_b >= 0 and signed_result >= 0)) {
+                    self.statusreg |= self.FLAG_V;
+                }
+            },
+            0b101 => {
+                const sa = (i32)(a);
+                result = (u32)(sa >> (b & 0x1F));
+            },
+            _ => result = 0,
+        }
+    }
+}
+
 fn fetch(self: *SoC) u32 {
     var word: u32 = 0;
     word = (self.instruction_memory[self.pc + 0] << 0) |
@@ -41,42 +120,11 @@ fn execR(self: *SoC, instr: u32) void {
     const fn7 = (instr >> 15) & 0b1111111; // fn7
     const fn3 = (instr >> 7) & 0b111; // fn3
 
-    if (fn7 == 0x0) {
-        switch (fn3) {
-            0x0 => self.regs[rd] = self.regs[rm] + self.regs[rn], // add
-            0x1 => self.regs[rd] = self.regs[rm] | self.regs[rn], // or
-            0x2 => self.regs[rd] = self.regs[rm] & self.regs[rn], // and
-            0x3 => self.regs[rd] = self.regs[rm] ^ self.regs[rn], // xor
-            0x4 => self.regs[rd] = self.regs[rm] << self.regs[rn], // lsl
-            0x5 => self.regs[rd] = self.regs[rm] >> self.regs[rn], // lsr
-            0x6 => {
-                if (self.regs[rm] == self.regs[rn]) {
-                    self.statusreg |= self.FLAG_EQ;
-                } else {
-                    self.statusreg &= !self.FLAG_EQ;
-                }
-
-                if (self.regs[rm] > self.regs[rn]) {
-                    self.statusreg |= self.FLAG_GT;
-                } else {
-                    self.statusreg &= !self.FLAG_GT;
-                }
-
-                if (self.regs[rm] < self.regs[rn]) {
-                    self.statusreg |= self.FLAG_LT;
-                } else {
-                    self.statusreg &= !self.FLAG_LT;
-                }
-            },
-        }
-    }
+    self.regs[rd] = ALU(self, rm, rn, fn3, fn7);
 }
 
 fn execI(self: *SoC, instr: u32) void {
-    const rm = (instr >> 27) & 0b11111;
-    const imm = (instr >> 15) & 0b1111_1111_1111;
-    const rd = (instr >> 10) & 0b11111;
-    const fn3 = (instr >> 7) & 0b111;
+    const rm = (instr  >>)
 }
 
 pub fn main() !void {}
